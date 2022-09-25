8ee3f90b5820a76bd2d2f381cc55ab52660ec2fc
penahkerimov99 <104515090+penahkerimov99@users.noreply.github.com>
2022-09-25 15:05:13 +0400

    slider2 hazirdi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom'
+import Slider2 from './components/Slider2';
+
 function App() {
   return (
     <BrowserRouter>
--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import './slider2.css'
+import '../index.css'
+import { Link } from 'react-router-dom'
+import berdroom from '../assets/img/bedroom.png'
+import chair from '../assets/img/chair.png'
+import dinning from '../assets/img/dinning.png'
+import lounge from '../assets/img/lounge.png'
+
+
 
 const Slider2 = () => {
   return (
-    <div>Slider2</div>
+    <div className='container-fluid d-flex justify-content-center'>
+      <div className='row'>
+        <Link to='/categories' className='col-2 p-5 d-flex justify-content-center align-items-center flex-column text-decoration-none'>
+          <img src={berdroom} alt='ddc' />
+          <span className='p-3'>Bedroom</span>
+        </Link>
+
+        <Link to='/categories' className='col-2 p-5 d-flex justify-content-center align-items-center flex-column text-decoration-none'>
+          <img src={chair} alt='ddc' />
+          <span className='p-3'>Living</span>
+        </Link>
+        
+        <Link to='/categories' className='col-2 p-5 d-flex justify-content-center align-items-center flex-column text-decoration-none'>
+          <img src={dinning} alt='ddc' />
+          <span className='p-3'>Dining</span>
+        </Link>
+
+        <Link to='/categories' className='col-2 p-5 d-flex justify-content-center align-items-center flex-column text-decoration-none'>
+          <img src={lounge} alt='ddc' />
+          <span className='p-3'>Lounge</span>
+        </Link>
+
+        <Link to='/categories' className='col-2 p-5 d-flex justify-content-center align-items-center flex-column text-decoration-none'>
+          <img src={chair} alt='ddc' />
+          <span className='p-3'>Office</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
