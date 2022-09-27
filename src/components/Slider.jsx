0f112c17ff58096354e5e9489c62b63e7bece026
Atayev <atayev25@gmail.com>
2022-09-27 20:23:29 +0400

    fix

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -44,17 +44,6 @@ console.log(data.slider[0]);
                           <img src={item?.link} alt={item?.name} className="chairsl"/>
                         </div>
                       </div>
-                      <div className="px-5 carousel-item d-flex">
-                        <div className="col col-md-6 col-sm-6 mt-5">
-                          <span className=' px-5 mx-3 fs-5'>{item?.title}</span>
-                          <h1 className=' px-5 mx-3 fw-bold fs-1 my-3'>{item?.name}</h1>
-                          <p className=' px-5 mx-3 my-3'>{item?.description}</p>
-                          <button className='my-3 bg-btn py-3 px-5 mx-5'>SHOP NOW</button>
-                        </div>
-                        <div className="col col-md-6 chair mt-5 col-sm-6">
-                          
-                        </div>
-                      </div>
                       </div> 
                   </React.Fragment>
                         ))
